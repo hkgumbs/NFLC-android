4745b879994a6ca6f3ab7cc3605493b519793431
hkgumbs <h.kofigumbs@gmail.com>
2014-04-12 14:01:50 -0400

    first UI iteration

--- src/edu/umd/nflc/world_cup/SplashActivity.java
+++ src/edu/umd/nflc/world_cup/SplashActivity.java
@@ -1,16 +1,28 @@
 package edu.umd.nflc.world_cup;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Window;
 
 public class SplashActivity extends Activity {
+
+	// DELAY IN MILISECONDS BEFORE LAUNCHING MAIN
+	private static int DELAY = 2000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				startActivity(new Intent(SplashActivity.this, MainActivity.class));
+			}
+		}, DELAY);
 	}
 
 }
